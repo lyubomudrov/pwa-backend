54448f8ca42c92705e8c6da0ea70121d0a92fc6b
Ardor Gaming <denislebedev1000023@gmail.com>
2026-04-12 19:04:09 +0300

    Fix

--- src/main/java/com/example/pwa_shop/controller/UserController.java
+++ src/main/java/com/example/pwa_shop/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.example.pwa_shop.controller;
 
-import com.example.pwa_shop.dto.CreateUserRequestDto;
 import com.example.pwa_shop.dto.UserResponseDto;
 import com.example.pwa_shop.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +15,25 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
-    public UserResponseDto create(@RequestBody CreateUserRequestDto request) {
-        return userService.create(request);
-    }
-
-    @GetMapping("/{id}")
-    public UserResponseDto getById(@PathVariable Long id) {
-        return userService.getByIdDto(id);
+    @GetMapping("/me")
+    public UserResponseDto getMe() {
+        return userService.getCurrentUserDto();
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserResponseDto> getAll() {
         return userService.getAll();
     }
 
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public UserResponseDto getById(@PathVariable Long id) {
+        return userService.getByIdDto(id);
+    }
+
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void delete(@PathVariable Long id) {
         userService.delete(id);
     }
